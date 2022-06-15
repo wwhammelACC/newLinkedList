5eeea68affdefd53c361948b68f093acb5ed5b4a
wwhammelACC <89032402+wwhammelACC@users.noreply.github.com>
2022-06-15 10:47:56 -0500

    added constructor and destructor

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,3 +1,27 @@
 /*
  * your comment header here
  */
+
+#include "linkedlist.h"
+
+/*
+* ****************************************
+* Constructors / Destructors
+* ****************************************
+*/
+
+//Constructor
+LinkedList::LinkedList(){
+    head = NULL;
+}
+
+//Destructor
+LinkedList::~LinkedList(){
+    clearList();
+}
+
+// PUBLIC METHODS
+
+
+
+// PRIVATE METHODS
