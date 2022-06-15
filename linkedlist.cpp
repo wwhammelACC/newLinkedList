e34afe8212a6d59d7b00d778caa7fdfe422368eb
wwhammelACC <89032402+wwhammelACC@users.noreply.github.com>
2022-06-15 10:58:18 -0500

    added stubbed out methods from linkedlist.h with test lines

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -22,6 +22,43 @@ LinkedList::~LinkedList(){
 
 // PUBLIC METHODS
 
+bool LinkedList::addNode(int id){
+    bool flag = false;
+    cout << "addNode Test Line " << endl;
+    cout << endl;
+}
 
+bool LinkedList::deleteNode(int id, string* info){
+    bool flag = false;
+    cout << "deleteNode Test Line " << endl;
+    cout << endl;
+}
+
+bool LinkedList::getNode(int id, Data* info){
+    bool flag = false;
+    cout << "getNode Test Line " << endl;
+    cout << endl;
+}
+
+void LinkedList::printList(bool direction){
+    cout << "printList Test Line " << endl;
+    cout << endl;
+}
+
+int LinkedList::getCount(){
+    cout << "getCount Test Line " << endl;
+       cout << endl;
+}
+
+bool LinkedList::clearList(){
+    cout << "clearList Test Line " << endl;
+       cout << endl;
+}
+
+bool LinkedList::exists(int id){
+    flag = false;
+    cout << "exists Test Line " << endl;
+    cout << endl;
+}
 
 // PRIVATE METHODS
